e47a1acf788368f6db98e3c2866dc896a04e0b1a
AmourDeMai <fengyh7@gmail.com>
2015-12-11 17:29:09 +0100

    bag of word in python

---
+++ BagOfWord/bag_of_words.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# train and predict, based on validation params
+
+import numpy as np
+import pandas as pd
+
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.linear_model import LogisticRegression as LR
+
+from KaggleWord2VecUtility import KaggleWord2VecUtility
+
+#
+
+train_file = 'data/train_v' 
+test_file = 'data/test_c'
+output_file = 'data/bow_predictions.csv'
+
+#
+
+train = pd.read_csv( train_file, header = 0, delimiter = "\t", quoting = 3 )
+test = pd.read_csv( test_file, header = 0, delimiter = "\t", quoting = 3 )
+
+#
+
+print "Parsing train reviews..."
+
+clean_train_reviews = []
+for review in train['TWITTER_MESSAGE']:
+	clean_train_reviews.append( " ".join( KaggleWord2VecUtility.review_to_wordlist( review )))
+
+print "Parsing test reviews..."
+
+clean_test_reviews = []
+for review in test['TWITTER_MESSAGE']:
+	clean_test_reviews.append( " ".join( KaggleWord2VecUtility.review_to_wordlist( review )))
+print len(clean_test_reviews)
+
+#
+
+print "Vectorizing train..."
+
+vectorizer = TfidfVectorizer( max_features = 40000, ngram_range = ( 1, 2 ), 
+	sublinear_tf = True )
+train_x = vectorizer.fit_transform( clean_train_reviews)
+
+print "Vectorizing test..."
+
+test_x = vectorizer.transform( clean_test_reviews )
+for elem in test_x:
+	print elem
+print "Training..."
+
+model = LR()
+model.fit( train_x, train["positive|negative|neutral|objective"] )
+p = model.predict_proba( test_x )[:,1] 
+
+#
+
+print "Writing results..."
+
+output = pd.DataFrame( data = { "Message": test["SID"], "sentiment": p } )
+output.to_csv( output_file, index = False, quoting = 3 )
+
+
+
